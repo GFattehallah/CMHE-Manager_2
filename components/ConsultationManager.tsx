7012417f440793e28dc8bd3666e47dd25ab292f7
GFattehallah <f.ghadi@uiz.ac.ma>
2026-01-16 23:24:59 +0100

    Update ConsultationManager.tsx

--- components/ConsultationManager.tsx
+++ components/ConsultationManager.tsx
@@ -270,7 +270,7 @@ export const ConsultationManager: React.FC = () => {
                                 </button>
                             </div>
                         </div>
-
+                      
                         <div className="bg-white p-4 rounded-xl border border-slate-200 shadow-sm">
                             <div className="flex justify-between items-center mb-2">
                                 <label className="block text-sm font-medium text-slate-700">Médicaments</label>
