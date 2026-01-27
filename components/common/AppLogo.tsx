0ee572a31ac200757cb1a7f9a35ba88201cf32f6
GFattehallah <f.ghadi@uiz.ac.ma>
2026-01-27 21:58:39 +0100

    Update AppLogo.tsx

--- components/common/AppLogo.tsx
+++ components/common/AppLogo.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { LOGO_URL } from '../../constants';
 
 interface AppLogoProps {
-  size?: number;
   className?: string;
+  showText?: boolean;
 }
 
 export const AppLogo: React.FC<AppLogoProps> = ({
-  size = 140,
-  className = '',
+  className = "",
+  showText = false
 }) => {
-  return (
-    <img
-      src={LOGO_URL}
-      alt="Logo"
-      style={{ width: size, height: 'auto' }}
-      className={`mx-auto ${className}`}
-    />
-  );
+
+const [imageError, setImageError] = useState(false);
+const [resolvedUrl, setResolvedUrl] = useState<string>(LOGO_URL);
+
+useEffect(() => {
+  try {
+    const href = window.location.href.split('#')[0];
+    const base = href.endsWith('/') ? href : href.substring(0, href.lastIndexOf('/') + 1);
+    const cleanLogoPath = LOGO_URL.replace('./', '');
+    const url = new URL(cleanLogoPath, base);
+    setResolvedUrl(url.href);
+  } catch {
+    setResolvedUrl(LOGO_URL);
+  }
+}, []);
+
+return (
+<div className={`w-full h-full flex items-center justify-center ${className}`}>
+
+{!imageError ? (
+
+<img
+src={resolvedUrl}
+alt="Logo"
+className="max-w-full max-h-full object-contain"
+/>
+
+) : (
+
+<div className="w-full h-full bg-medical-900 text-white flex items-center justify-center font-black text-xs">
+CMHE
+</div>
+
+)}
+
+</div>
+);
 };
